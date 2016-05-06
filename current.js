e027eccfce9e590be2f91340f9811c25734496a6
johel <johel.carvalho@gmail.com>
2016-05-06 14:37:11 -0300

    implementing store from scratch

--- current.js
+++ current.js
@@ -10,7 +10,36 @@ function counter(state = 0, action){
  
 }
 
-const {createStore} = Redux;
+function createStore(reducer){
+  var listeners = [];
+  var state;
+  var store = {
+    getState:function(){
+      return state;
+    },
+    dispatch:function(action){
+      console.log('dispatch');
+      state = reducer(state, action);
+      console.log('state', state);
+      listeners.forEach((listener)=> listener());
+    },
+    subscribe:function(listener){
+      console.log('subscribe');
+      listeners.push(listener);
+      function unsubscribe(){
+        listeners = listeners.filter(item => item !== listener);
+      }
+      return unsubscribe;
+    }   
+  };
+  
+  store.dispatch({});
+  
+  return store;
+   
+}
+
+// const {createStore} = Redux;
 
 const store = createStore(counter);
 

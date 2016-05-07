20101ba95ea9a8e7c62deef44deb0343219400ed
johel <johel.carvalho@gmail.com>
2016-05-07 08:56:01 -0300

    toggle todo - avoiding object mutations with Object.assign and oject spread

--- current.js
+++ current.js
@@ -1,44 +1,27 @@
-var deepFreeze = window.deepFreeze;
-const addCounter = (list) => {
-//   list.push(0);
-  return [...list, 0];
+const toggleTodo = (todo) => {
+    //ES6
+	//return Object.assign({}, todo, {completed:!todo.completed});
+  
+    //ES7 Object Spread
+    return {...todo, completed: !todo.completed};
 }
 
-const removeCounter = (list, index) => {
-  return [
-    ...list.slice(0,index), ...list.slice(index+1)
-  ];
+const testToggleTodo = () => {
+	const todoBefore = {
+		id:1,
+		text:'text',
+		completed:true
+	};
+	const todoAfter = {
+		id:1,
+		text:'text',
+		completed:false
+	};
+  
+    deepFreeze(todoBefore)
+    
+	expect(toggleTodo(todoBefore)).toEqual(todoAfter);
 }
 
-const incrementCounter = (list, index) => {
-  return [
-    ...list.slice(0,index),list[index] + 1, ...list.slice(index+1)
-  ];
-}
-
-const testAddCounter = () => {
-  listBefore = [];
-  listAfter = [0];
-  deepFreeze(listBefore)
-  expect(addCounter(listBefore)).toEqual(listAfter);
-}
-
-const testRemoveCounter = () => {
-  listBefore = [1,2,3];
-  listAfter = [1,3];
-  deepFreeze(listBefore)
-  expect(removeCounter(listBefore, 1)).toEqual(listAfter);
-}
-
-const testIncrementCounter = () => {
-  listBefore = [1,2,3];
-  listAfter = [1,2,4];
-  deepFreeze(listBefore);
-  expect(incrementCounter(listBefore,2)).toEqual(listAfter);
-}
-
-testAddCounter();
-testRemoveCounter();
-testIncrementCounter();
-
-console.log("deu certo")
+testToggleTodo();
+console.log('deu certo');
